246de9c533a2f8891fc96e4991ee37868e4b7db1
Bartosz Tyrcha <296630@stud.umk.pl>
2021-04-20 18:33:12 +0200

    wykresy

--- plot_histogram_cord.py
+++ plot_histogram_cord.py
@@ -4,9 +4,9 @@ import matplotlib.pyplot as plt
 time, dist, cord = np.loadtxt("irtg-school-mainz-2020/COLVAR-driver", unpack=True)
 
 plt.figure()
-hist = plt.hist(dist, bins=100, density=True, color="olivedrab")
-plt.xlabel("Odległość pomiędzy Na-Cl [nm]")
-plt.title("Histogram dla odległości Na-Cl")
+hist = plt.hist(cord, bins=100, density=True, color="olivedrab")
+plt.xlabel("Liczba koordynacyjna")
+plt.title("Histogram dla liczby koordynacyjnej")
 
 temp = 300  # K
 #k = 0.0083144626  # kJ/(mol*K)
@@ -23,8 +23,8 @@ for i in range(len(bins) - 1):
 
 plt.figure()
 plt.plot(dist_prime, F, color="olivedrab")
-plt.xlabel("Odległość pomiędzy Na-Cl [nm]")
+plt.xlabel("Liczba koordynacyjna")
 plt.ylabel("F [eV]")
-plt.title("Energia swobodna dla odległości Na-Cl")
+plt.title("Energia swobodna dla liczby koordynacyjnej")
 
 plt.show()
