5881f43fcb71a016a2d6b7ace7af00ee902703a0
Bartosz Tyrcha <296630@stud.umk.pl>
2021-04-20 17:03:41 +0200

    Energia swobodna

--- plot_histogram.py
+++ plot_histogram.py
@@ -4,7 +4,22 @@ import matplotlib.pyplot as plt
 time, dist, cord = np.loadtxt("irtg-school-mainz-2020/COLVAR-driver", unpack=True)
 
 plt.figure()
-plt.hist(dist, bins=100, density=True, color="olivedrab")
+hist = plt.hist(dist, bins=100, density=True, color="olivedrab")
 plt.xlabel("Odległość pomiędzy Na-Cl [nm]")
 plt.title("Histogram")
+#plt.show()
+
+temp = 300  # K
+#k = 0.0083144626  # kJ/(mol*K)
+k = 8.6173333e-05  # eV/K
+
+N = hist[0]
+bins = hist[1]
+
+F = - np.log(N) * k * temp
+
+plt.figure()
+plt.hist(bins[:-1], bins=bins, weights=F)
+plt.xlabel("Odległość pomiędzy Na-Cl [nm]")
+plt.ylabel("F [eV]")
 plt.show()
